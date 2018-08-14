76cb09cdf5706721f1df809780d8c9edde381550
uncoan <940414630@qq.com>
2018-08-14 15:43:23 +0800

    first commit

---
+++ src/main/java/com/example/mapper/AuthorMapper.java
@@ -0,0 +1,12 @@
+package com.example.mapper;
+
+import java.util.List;
+
+import com.example.model.Author;
+
+public interface AuthorMapper {
+	List<Author> findAllAuthors();
+	Author findOneAuthor(int id);
+	int insertAuthor(Author author);
+	int deleteAuthor(int id);
+}
